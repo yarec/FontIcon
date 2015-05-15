b0086888f7a1de7a7bde84ca880939b2ffc1d5eb
yarec <yarec@163.com>
2015-05-15 11:38:31 +0800

    u

---
+++ FontIcon/FontIcon/FontIcon.h
@@ -0,0 +1,19 @@
+//
+//  FontIcon.h
+//  FontIcon
+//
+//  Created by r.t on 15/5/15.
+//  Copyright (c) 2015年 r.t. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontIcon.
+FOUNDATION_EXPORT double FontIconVersionNumber;
+
+//! Project version string for FontIcon.
+FOUNDATION_EXPORT const unsigned char FontIconVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontIcon/PublicHeader.h>
+
+
